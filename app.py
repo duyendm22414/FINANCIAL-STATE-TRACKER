9d3d83ea2d4fc86a2df9aa5327446fd6c40e0810
duyendm22414 <duyendm22414@st.uel.edu.vn>
2026-02-27 17:46:08 +0700

    app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.set_page_config(page_title="Financial State Tracker", layout="wide")
 
 pages = [
-    st.Page("pages/00_Giới_thiệu_sản_phẩm.py", title="Giới thiệu sản phẩm"),
-    st.Page("pages/01_Tổng_quan.py", title="Tổng quan"),
-    st.Page("pages/02_Ngành.py", title="Ngành"),
-    st.Page("pages/03_Doanh_nghiệp.py", title="Doanh nghiệp"),
-    st.Page("pages/04_Phương_pháp.py", title="Phương pháp"),
-    st.Page("pages/05_Tải_dữ_liệu.py", title="Tải dữ liệu"),
+    st.Page("pages/00_Giới_thiệu_sản_phẩm.py", title="GIỚI THIỆU"),
+    st.Page("pages/01_Tổng_quan.py", title="TỔNG QUAN"),
+    st.Page("pages/02_Ngành.py", title="NGÀNH"),
+    st.Page("pages/03_Doanh_nghiệp.py", title="DOANH NGHIỆP"),
+    st.Page("pages/04_Phương_pháp.py", title="PHƯƠNG PHÁP"),
+    st.Page("pages/05_Tải_dữ_liệu.py", title="DỮ LIỆU"),
 ]
 
 nav = st.navigation(pages)
